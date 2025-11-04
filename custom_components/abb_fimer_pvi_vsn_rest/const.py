dc7ad1bda07c356a33354fdc6cfb52a5a6745ce6
Alessandro Del Prete <alessandro.delprete@gmail.com>
2025-11-04 13:16:11 +0100

    chore(release): bump version to v1.0.0-beta.16

--- custom_components/abb_fimer_pvi_vsn_rest/const.py
+++ custom_components/abb_fimer_pvi_vsn_rest/const.py
@@ -1,7 +1,7 @@
 """Constants for ABB FIMER PVI VSN REST integration."""
 
 DOMAIN = "abb_fimer_pvi_vsn_rest"
-VERSION = "1.0.0-beta.15"
+VERSION = "1.0.0-beta.16"
 
 # Configuration
 CONF_HOST = "host"
